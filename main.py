c463d2cfef58570eb12ecf1dc669608fe31342d9
adao <bboyadao@gmail.com>
2019-04-11 21:22:50 +0700

    first commit

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-import threading
-import zipfile
-from PyPDF2 import PdfFileMerger
-import subprocess
-from docx.shared import Cm
-import comtypes.client
+import os
 import sys
-from shutil import copy2, copytree, rmtree, move
+import csv
+import time
+import zipfile
 import pathlib
 import requests
-from io import BytesIO
-from distutils.dir_util import copy_tree
-import csv
-from docx import Document
-from docxcompose.composer import Composer
-import os
+import subprocess
 from os import listdir
+import comtypes.client
+from io import BytesIO
+from docx import Document
+from PyPDF2 import PdfFileMerger
 from os.path import isfile, join
+from distutils.dir_util import copy_tree
+from shutil import (copy2, copytree, rmtree, move)
+
+
 wdFormatPDF = 17
 
 
@@ -27,13 +27,6 @@ def zipdir(path, ziph):
         folder = root[length:]  # path without "parent"
         for file in files:
             ziph.write(os.path.join(root, file), os.path.join(folder, file))
-#
-# def zipdir(path, ziph):
-#     # ziph is zipfile handle
-#     for root, dirs, files in os.walk(path):
-#         for file in files:
-#             ziph.write(os.path.join(root, file))
-#
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -47,22 +40,105 @@ onlyfiles = [f for f in listdir(templates) if isfile(join(templates, f))]
 
 onlyfolder = [f for f in listdir(templates) if not isfile(join(templates, f))]
 
+# def convert_to_pdf(src, dst):
+#     p = subprocess.Popen(["soffice", "--headless", "--convert-to", "pdf",
+#                           src, "--outdir", dst])
+#     (output, err) = p.communicate()
+#     p_status = p.wait()
 
+
+# def merge_pdf(pdfs, dst):
+#     merger = PdfFileMerger()
+#     for pdf in pdfs:
+#         merger.append(open(pdf, 'rb'))
+#
+#     with open(dst, 'wb') as fout:
+#         merger.write(fout)
+#
 def update_toc(docx_file):
     word = comtypes.client.CreateObject("Word.Application")
     doc = word.Documents.Open(docx_file)
+    time.sleep(5)
     toc_count = doc.TablesOfContents.Count
-    if toc_count == 1:
+
+    if toc_count > 0:
         toc = doc.TablesOfContents(1)
         toc.Update
         print('TOC should have been updated.')
     else:
-        print('TOC has not been updated for sure...')
-    print(docx_file)
-    doc.SaveAs(docx_file,FileFormat=16)
+        print('TOC has not been updated...')
+
+    doc.Save()
+    doc.SaveAs(docx_file, FileFormat=16)
     doc.Close(SaveChanges=True)
     # word.Close()
     word.Quit()
+
+
+def merged_by_macro(clone, merged_name):
+    macro = r'''
+Sub NewDocWithCode()
+    Dim doc As Document
+    Set doc = ActiveDocument
+    doc.VBProject.VBComponents("ThisDocument").CodeModule.AddFromString _
+      "Sub Mergedocuments()" & vbLf & _
+      "Application.ScreenUpdating = False" & vbLf & _
+      "MyPath = ActiveDocument.Path" & vbLf & _
+      "MyName = Dir(MyPath & ""\"" & ""*.docx"")" & vbLf & _
+      "i = 0 " & vbLf & _
+      "Do While MyName <> """"" & vbLf & _
+      "If MyName <> ActiveDocument.Name Then " & vbLf & _
+      "Set wb = Documents.Open(MyPath & ""\"" & MyName)" & vbLf & _
+      "Selection.WholeStory" & vbLf & _
+      "Selection.Copy" & vbLf & _
+      "Windows(1).Activate" & vbLf & _
+      "Selection.EndKey Unit:=wdLine" & vbLf & _
+      "Selection.TypeParagraph" & vbLf & _
+      "Selection.Paste " & vbLf & _
+      "i = i + 1 " & vbLf & _
+      "wb.Close False" & vbLf & _
+      "End If " & vbLf & _
+      "MyName = Dir" & vbLf & _
+      "Loop " & vbLf & _
+      "Application.ScreenUpdating = True" & vbLf & _
+      "End Sub "
+End Sub
+    '''
+    macr2 = """
+    Sub Macro1()
+    Selection.WholeStory
+    Selection.Delete Unit:=wdCharacter, Count:=1
+    End Sub
+    """
+    word = comtypes.client.CreateObject("Word.Application")
+    doc = word.Documents.Open(clone)
+    wordModule = doc.VBProject.VBComponents.Add(1)
+    wordModule.CodeModule.AddFromString(macr2)
+    word.Application.Run("Macro1")
+    doc.Save()
+    doc.SaveAs(clone, FileFormat=16)
+    doc.Close()
+    word.Quit()
+
+    word = comtypes.client.CreateObject("Word.Application")
+
+    doc = word.Documents.Open(clone)
+    time.sleep(5)
+    wordModule = doc.VBProject.VBComponents.Add(1)
+    wordModule.CodeModule.AddFromString(macro)
+    word.Application.Run("NewDocWithCode")
+    time.sleep(1)
+    word.Application.Run("Mergedocuments")
+   # time.sleep(30)
+    doc.Save()
+    doc.SaveAs(merged_name, FileFormat=16)
+    doc.Close()
+    word.Quit()
+    _path = os.path.dirname(clone)
+    _files = [f for f in listdir(_path) if isfile(join(_path, f))]
+    for i in _files:
+        i = os.path.join(_path, i)
+    return _files
 
 
 def replace_word(path, cp_name, position, industry, logo):
@@ -104,41 +180,6 @@ def replace_word(path, cp_name, position, industry, logo):
     return doc
 
 
-def combine_word_documents(files, merged_name):
-
-    merged_document = Document()
-
-    for index, file in enumerate(files):
-        sub_doc = Document(file)
-
-        # Don't add a page break if you've reached the last file.
-        if index < len(files) - 1:
-            sub_doc.add_page_break()
-
-        for element in sub_doc.element.body:
-            merged_document.element.body.append(element)
-
-    merged_document.save(merged_name)
-
-
-def merged_docx(files, merged_name):
-    print(files)
-    master = Document()
-    composer = Composer(master)
-    for i in files:
-        composer.append(Document(i), remove_property_fields=False)
-    composer.save(os.path.join(src_path, merged_name))
-    new_doc = Document(merged_name)
-    for i in new_doc.sections:
-        i.left_margin = Cm(1.5)
-        i.right_margin = Cm(1.5)
-
-    new_doc.save(merged_name)
-
-    # update_toc(merged_name)
-    return merged_name
-
-
 def get_form(path):
 
     with open(os.path.join(src_path, path)) as f:
@@ -157,11 +198,11 @@ def convert_pptx_to_pdf(src, dst):
 
 
 def convert_to_pdf(src, dst):
-    
-    dst=dst.replace("docx","pdf" )
+
+    dst = dst.replace("docx", "pdf")
     word = comtypes.client.CreateObject('Word.Application')
     word.Visible = False
-    #word.DisplayAlerts = False
+    # word.DisplayAlerts = False
     doc = word.Documents.Open(src)
     doc.SaveAs(dst, FileFormat=wdFormatPDF)
     doc.Close()
@@ -169,20 +210,19 @@ def convert_to_pdf(src, dst):
     return True
 
 
-# def convert_to_pdf(src, dst):
-#     p = subprocess.Popen(["soffice", "--headless", "--convert-to", "pdf",
-#                           src, "--outdir", dst])
-#     (output, err) = p.communicate()
-#     p_status = p.wait()
+def create_sys_temp_dir(files, sys_temp_dir):
+    for i, j in enumerate(files):
+        a = f"{i}-{os.path.basename(j)}"
+        clone = f"copy_template.docx"
+        if i == 0:
 
+            copy2(j, os.path.join(sys_temp_dir, a))
+            copy2(j, os.path.join(sys_temp_dir, clone))
 
-def merge_pdf(pdfs, dst):
-    merger = PdfFileMerger()
-    for pdf in pdfs:
-        merger.append(open(pdf, 'rb'))
+        else:
+            copy2(j, os.path.join(sys_temp_dir, a))
 
-    with open(dst, 'wb') as fout:
-        merger.write(fout)
+    return sys_temp_dir
 
 
 if __name__ == '__main__':
@@ -206,6 +246,9 @@ if __name__ == '__main__':
         pathlib.Path(temp_dir).mkdir(parents=True, exist_ok=True)
 
         pathlib.Path(os.path.join(parent_dir, "Study Guide")
+                     ).mkdir(parents=True, exist_ok=True)
+
+        pathlib.Path(os.path.join(temp_dir, "sys_temp_dir")
                      ).mkdir(parents=True, exist_ok=True)
 
         pathlib.Path(os.path.join(parent_dir, "Course")
@@ -279,7 +322,6 @@ if __name__ == '__main__':
         now_only_files = [f for f in listdir(
             new_dir) if isfile(join(new_dir, f))]
 
-
         only_files_docx = [f for f in now_only_files if "docx" in f]
 
         only_files_docx = [f for f in only_files_docx if f[:2] != "~$"]
@@ -292,9 +334,6 @@ if __name__ == '__main__':
         workbook_list = []
         study_file.sort()
         # repalce method
-        for i in only_files_docx:
-            replace_word(os.path.join(new_dir, i),
-                         cp_name, position, industry, logo)
 
         study_file.insert(1, f"{interviewprocess}.docx")
         study_file.insert(3, f"{industry}.docx")
@@ -315,22 +354,33 @@ if __name__ == '__main__':
 
         pdf_study_list = [i.replace("docx", "pdf") for i in study_list]
 
+        list_temp_dir = create_sys_temp_dir(
+            study_list, os.path.join(temp_dir, "sys_temp_dir"))
+        _stu = os.path.join(
+            temp_dir, f"Study Guide–{cp_name} {position} Interview preparation.docx")
+        print(
+            f"========== MERGING: {_stu}... ====================")
+        print(f"== == == == == MERGING: Macro")
+
+        list_files_in_temp = merged_by_macro(os.path.join(
+            temp_dir, "sys_temp_dir", "copy_template.docx"), merged_study)
+
+        replace_word(os.path.join(new_dir, _stu),
+                     cp_name, position, industry, logo)
+        update_toc(_stu)
         print(
             f"========== MERGING: Study Guide–{cp_name} {position} Interview preparation.pdf... ====================")
-        for i in study_list:
-            convert_to_pdf(i, os.path.join(new_dir, os.path.basename(i)))
-        merge_pdf(pdf_study_list, merged_study_pdf)
 
-        print(
-            f"========== MERGING: Study Guide–{cp_name} {position} Interview preparation.docx... ====================")
-        merged_docx(
-            study_list, merged_study)
+        convert_to_pdf(merged_study, merged_study_pdf)
 
+        pathlib.Path(os.path.join(temp_dir, "sys_temp_dir")
+                     ).mkdir(parents=True, exist_ok=True)
         print(
             f"========== MERGING: Done ====================")
 
         copy2(merged_study_pdf, os.path.join(
             parent_dir, "Course", f'Study Guide–{cp_name} {position} Interview preparation.pdf'))
+
         copy2(merged_study_pdf, os.path.join(
             parent_dir, "Study Guide", f'Study Guide–{cp_name} {position} Interview preparation.pdf'))
 
@@ -341,22 +391,28 @@ if __name__ == '__main__':
         workbook_file.insert(9, f"{questionsandanswers}.docx")
         for i in workbook_file:
             workbook_list.append(os.path.join(new_dir, i))
+        _work = os.path.join(
+            temp_dir, f"Workbook–{cp_name} {position} Interview preparation.docx")
         print(
-            f"========== MERGING: Workbook–{cp_name} {position} Interview preparation.docx... ====================")
-        merged_docx(
-            workbook_list, merged_workbook)
+            f"========== MERGING: {_work}... ====================")
 
-        for i in workbook_list:
-            convert_to_pdf(i, new_dir)
+        for i in list_files_in_temp:
+            os.remove(i)
 
-        pdf_workbook_list = [i.replace("docx", "pdf") for i in study_list]
-        merge_pdf(pdf_workbook_list, merged_workbook_pdf)
+        list_files_in_temp = merged_by_macro(os.path.join(
+            temp_dir, "sys_temp_dir", "copy_template.docx"), merged_workbook)
+
+        replace_word(os.path.join(new_dir, _work),
+                     cp_name, position, industry, logo)
+        update_toc(_work)
+
+        convert_to_pdf(merged_workbook, merged_workbook_pdf)
 
         print(
             f"========== MERGING: Done ====================")
 
         copy2(merged_workbook_pdf, os.path.join(
-            parent_dir, "Course", f'Workbook––{cp_name} {position} Interview preparation.pdf'))
+            parent_dir, "Course", f'Workbook–{cp_name} {position} Interview preparation.pdf'))
         for i in onlyfiles:
             if "pdf" in i:
                 if f"Interview preparation" in i:
@@ -385,3 +441,4 @@ if __name__ == '__main__':
             parent_dir, f"Course – {cp_name} {position} Interview preparation.zip"), 'w', zipfile.ZIP_DEFLATED)
         zipdir(os.path.join(parent_dir, "Course"), zipf)
         zipf.close()
+        rmtree(os.path.join(temp_dir, "sys_temp_dir"))
--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import shopify
 import ssl
+from docx import Document
+from docxcompose.composer import Composer
 
 ssl._create_default_https_context = ssl._create_unverified_context
 API_KEY, PASSWORD = "6413e468090c0e3acac67e564db39752", "7bd6e0bffac90790b6a6343d3042e923"
@@ -49,6 +51,70 @@ def create_product():
     success = new_product.save()
 
 
+def merge():
+    files = [
+        # '/Users/xiaoma/projects/shopify-book/src/Output/Amazon/Fulfillment Associate/Temp Files/Study Guide - Template 1.docx',
+        "/Users/xiaoma/projects/shopify-book/src/Templates/Study Guide - Template 1 - tadaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.docx",
+        # '/Users/xiaoma/projects/shopify-book/src/Output/Amazon/Fulfillment Associate/Temp Files/amazon.docx', '/Users/xiaoma/projects/shopify-book/src/Output/Amazon/Fulfillment Associate/Temp Files/Study Guide - Template 2.docx',
+        # '/Users/xiaoma/projects/shopify-book/src/Output/Amazon/Fulfillment Associate/Temp Files/Internet Commerce.docx',
+        # '/Users/xiaoma/projects/shopify-book/src/Output/Amazon/Fulfillment Associate/Temp Files/Study Guide - Template 3.docx',
+        # '/Users/xiaoma/projects/shopify-book/src/Output/Amazon/Fulfillment Associate/Temp Files/Amazon.docx',
+        # '/Users/xiaoma/projects/shopify-book/src/Output/Amazon/Fulfillment Associate/Temp Files/Study Guide - Template 4.docx',
+        # '/Users/xiaoma/projects/shopify-book/src/Output/Amazon/Fulfillment Associate/Temp Files/Amazon Fulfillment Associate.docx',
+        '/Users/xiaoma/projects/shopify-book/src/Output/Amazon/Fulfillment Associate/Temp Files/Study Guide - Template 5.docx',
+        #  '/Users/xiaoma/projects/shopify-book/src/Output/Amazon/Fulfillment Associate/Temp Files/amazon_operations.docx', '/Users/xiaoma/projects/shopify-book/src/Output/Amazon/Fulfillment Associate/Temp Files/Study Guide - Template 6.docx'
+    ]
+    merged_name = "/Users/xiaoma/projects/shopify-book/src/Output/Amazon/Fulfillment Associate/Temp Files/tabbbbbbbbb.docx"
+    master = Document(files[0])
+    master1 = Document(files[1])
+    composer = Composer(master)
+    composer.append(master1)
+
+    composer.save(merged_name)
+
+    # composer.append(doc1, doc2)
+
+
+def mer2():
+    temp1 = '/Users/xiaoma/projects/shopify-book/src/Templates/Study Guide - Template 1.docx'
+    doc = Document(temp1)
+    return doc
+    foot = doc.sections[-1].footer
+
+    merged_name = "/Users/xiaoma/projects/shopify-book/src/Output/Amazon/Fulfillment Associate/Temp Files/tabbbbbbbbb.docx"
+    # doc1 = Document(merged_name).sections[-1].footer = foot
+    # doc1
+    # return doc
+    # print(doc.__dict__)
+
+
+def mer3():
+    temp1 = '/Users/xiaoma/projects/shopify-book/src/Templates/Study Guide - Template 1.docx'
+    doc = Document(temp1)
+    a = doc._body._body
+    print(dir(a))
+    for i in a:
+        print(i.text)
+
+    # a.replace("[", "fuckkkkkkkkk")
+    doc.save('/Users/xiaoma/projects/shopify-book/src/Templates/Study Guide - Template 1 - tadaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.docx')
+
+
 if __name__ == '__main__':
-    a = list_products()
-    print(a)
+    # a = merge()
+
+    # merge()
+    merged_name = '/Users/xiaoma/projects/shopify-book/src/Output/Amazon/Fulfillment Associate/Temp Files/Study Guide - Template 1.docx'
+    docx = mer2()
+    a = docx.sections[-1]
+    foot_src = a.footer
+    doc = Document(merged_name)
+    head = doc.sections[-1].header
+    head.add_header("ssssss")
+    footer = doc.sections[-1].footer
+    footer.paragraphs[0].text = "fuckkkkkkkkk"
+    # footer.add_paragraph(
+    # "ffffffffffffffffffffffffffffffffffffffffffffffffffffff")
+    doc.add_heading('Document Title', 0)
+
+    doc.save("/Users/xiaoma/projects/shopify-book/src/Output/Amazon/Fulfillment Associate/Temp Files/changefottt.docx")
